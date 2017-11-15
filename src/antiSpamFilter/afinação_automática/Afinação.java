7d1051b2689a9bec8118a74aabc35c70c4b622aa
Guilherme Azevedo <gssao@iscte-iul.pt>
2017-11-15 11:26:32 +0000

    função para obter as regras do ficheiro rules.cf

---
+++ src/antiSpamFilter/afinação_automática/Afinação.java
@@ -0,0 +1,26 @@
+package antiSpamFilter.afinação_automática;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Afinação {
+
+	public static String[] rules(String rules_path) {
+		List<String> rules = new ArrayList<>();
+		try {
+			Scanner scn = new Scanner(new File(rules_path));
+			while (scn.hasNextLine()) {
+				String line = scn.nextLine();
+				if (!line.equals(""))
+					rules.add(line);
+			}
+			scn.close();
+		} catch (FileNotFoundException e) { // if is not a file
+		}
+		return rules.toArray(new String[0]);
+	}
+
+}
