0445385b24970153cb013721c7aca2547bcafd6a
Ana Pestana <aspaa1@iscte-iul.pt>
2017-11-22 12:39:42 +0000

    Gerar um vetor de pesos com tamanho correto

--- src/antiSpamFilter/frames/AfinacaoAutomatica.java
+++ src/antiSpamFilter/frames/AfinacaoAutomatica.java
@@ -2,6 +2,7 @@ package antiSpamFilter.frames;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
@@ -109,7 +110,7 @@ public class AfinacaoAutomatica {
 		help_panel_1.add(help, BorderLayout.WEST);
 		JLabel label = new JLabel("Falsos Positivos (FP): FP/Total");
 		help_panel_1.add(label, BorderLayout.CENTER);
-		help_text1 = new JTextArea("Um Falso Positivo (FP) ocorre quando uma mensagem legítima é classificada como mensagem spam");
+		help_text1 = new JTextArea("Um Falso Positivo (FP) ocorre quando uma mensagem legítima é classificada como mensagem spam.");
 		help_text1.setForeground(new JPanel().getBackground());
 		help_text1.setBackground(new JPanel().getBackground());
 		help_text1.setLineWrap(true);
@@ -134,7 +135,7 @@ public class AfinacaoAutomatica {
 		help_panel_2.add(help, BorderLayout.WEST);
 		label = new JLabel("Falsos Negativos (FN): FN/Total");
 		help_panel_2.add(label, BorderLayout.CENTER);
-		help_text2 = new JTextArea("Um Falso Negativo (FN) ocorre quando uma mensagem spam é classificada como mensagem legítima");
+		help_text2 = new JTextArea("Um Falso Negativo (FN) ocorre quando uma mensagem spam é classificada como mensagem legítima.");
 		help_text2.setForeground(new JPanel().getBackground());
 		help_text2.setBackground(new JPanel().getBackground());
 		help_text2.setLineWrap(true);
@@ -151,88 +152,6 @@ public class AfinacaoAutomatica {
 		panel.add(p, BorderLayout.EAST);
 
 		frame.add(panel);
-
-		/*
-		 * p1.setBounds(0, n.getHeight(), x / 2, y - n.getHeight());
-		 * 
-		 * for (HashMap.Entry<String, Integer> pair : rules.entrySet()) {
-		 * model.addElement(pair.getKey() + "       " + pair.getValue()); }
-		 * 
-		 * // Iterator<Entry<String, Integer>> it = rules.entrySet().iterator();
-		 * // while(it.hasNext()){ // model.addElement("" + it.next()); // }
-		 * 
-		 * JScrollPane scrollPane = new JScrollPane(); list = new
-		 * JList<>(model); scrollPane.setViewportView(list);
-		 * scrollPane.setPreferredSize(new Dimension(228, 313));
-		 * scrollPane.setMinimumSize(new Dimension(228, 313));
-		 * scrollPane.setMaximumSize(new Dimension(228, 313));
-		 * list.setLayoutOrientation(JList.VERTICAL); list.setEnabled(false);
-		 * 
-		 * p1.add(scrollPane); frame.add(p1, BorderLayout.WEST);
-		 * 
-		 * 
-		 * // Panel 2 (Lado direito da frame, falta ajustar as posições)
-		 * 
-		 * JPanel p2 = new JPanel(); p2.setPreferredSize(new Dimension(x / 2,
-		 * y)); p2.setBounds(x / 2 + 1, 0, x, y); p2.setLayout(new BoxLayout(p2,
-		 * BoxLayout.Y_AXIS)); p2.add(Box.createVerticalGlue()); JLabel n1 = new
-		 * JLabel("Para a configuração gerada, obtemos:"); p2.add(n1);
-		 * 
-		 * JPanel p2h = new JPanel(); p2h.setLayout(new FlowLayout());
-		 * 
-		 * JButton help1 = new JButton("?"); p2h.add(help1);
-		 * 
-		 * JLabel l1 = new JLabel("Falsos Positivos (FP):   " + FP + "/100");
-		 * p2h.add(l1);
-		 * 
-		 * p2.add(p2h);
-		 * 
-		 * JTextArea text1 = new JTextArea(
-		 * "Um Falso Positivo (FP) ocorre quando uma mensagem legítima é classificada"
-		 * + " como mensagem spam"); text1.setLineWrap(true);
-		 * text1.setPreferredSize(new Dimension(190, 75));
-		 * text1.setMinimumSize(new Dimension(190, 75));
-		 * text1.setMaximumSize(new Dimension(190, 75)); p2.add(text1);
-		 * 
-		 * help1.addActionListener(new ActionListener() { public void
-		 * actionPerformed(ActionEvent arg0) {
-		 * text1.setVisible(!text1.isVisible()); text1.revalidate();
-		 * text1.repaint(); } });
-		 * 
-		 * JPanel p2h2 = new JPanel(); p2h2.setLayout(new FlowLayout());
-		 * 
-		 * JButton help2 = new JButton("?"); p2h2.add(help2);
-		 * 
-		 * JLabel l3 = new JLabel("Falsos Negativos (FN):   " + FN + "/100");
-		 * p2h2.add(l3);
-		 * 
-		 * p2.add(p2h2);
-		 * 
-		 * JTextArea text2 = new JTextArea(
-		 * "Um Falso Positivo (FP) ocorre quando uma mensagem legítima é classificada"
-		 * + " como mensagem spam"); text2.setLineWrap(true); p2.add(text2);
-		 * 
-		 * text2.setPreferredSize(new Dimension(190, 75));
-		 * text2.setMinimumSize(new Dimension(190, 75));
-		 * text2.setMaximumSize(new Dimension(190, 75));
-		 * 
-		 * help2.addActionListener(new ActionListener() { public void
-		 * actionPerformed(ActionEvent arg0) {
-		 * text2.setVisible(!text2.isVisible()); text2.revalidate();
-		 * text2.repaint(); } });
-		 * 
-		 * frame.add(p2, BorderLayout.CENTER);
-		 * 
-		 * // panel 3 (Botoes no fim da frame, falta adicionar as
-		 * funcionalidades)
-		 * 
-		 * JPanel p3 = new JPanel(); p3.setLayout(new
-		 * FlowLayout(FlowLayout.RIGHT)); JButton gerar = new
-		 * JButton("Gerar novo vetor de pesos"); p3.add(gerar);
-		 * 
-		 * JButton cancelar = new JButton("Cancelar"); p3.add(cancelar);
-		 * frame.add(p3, BorderLayout.PAGE_END);
-		 */
 	}
 
 	public JButton helpButton() {
