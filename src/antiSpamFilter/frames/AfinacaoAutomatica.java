f22b54605f43760de56bc5535038129a095ef7ee
Guilherme Azevedo <gssao@iscte-iul.pt>
2017-12-06 16:19:28 +0000

    hiper volume feito

--- src/antiSpamFilter/frames/AfinacaoAutomatica.java
+++ src/antiSpamFilter/frames/AfinacaoAutomatica.java
@@ -36,11 +36,7 @@ import antiSpamFilter.utils.Utils;
 public class AfinacaoAutomatica {
 
 	private static JFrame afinacaoAuto;
-//	private JTextArea help_text_fp, help_text_fn;
 	private String help_label_fp, help_label_fn;
-//	private JScrollPane scroll_rules_panel;
-//	private Font font_titles = new Font("Helvetica", Font.PLAIN, 18),
-//			font_labels = new Font("Helvetica", Font.PLAIN, 14), font_text = new Font("Helvetica", Font.PLAIN, 12);
 
 	/**
 	 * Construtor da página de Afinação Automática
@@ -92,42 +88,6 @@ public class AfinacaoAutomatica {
 		JPanel panel = new JPanel();
 		
 		JPanel center_panel = GuiUtils.constructGUI(panel, help_label_fp, help_label_fn);
-//		panel.setLayout(new BorderLayout());
-//		panel.setBorder(new EmptyBorder(20, 20, 20, 20));
-//
-//		JPanel center_panel = new JPanel();
-//		center_panel.setLayout(new BorderLayout());
-//
-//		JLabel panelTitle = new JLabel("Configuração do vetor de pesos");
-//		panelTitle.setFont(font_titles);
-//		center_panel.add(panelTitle, BorderLayout.NORTH);
-//
-//		createRulesPanel();
-//		center_panel.add(scroll_rules_panel, BorderLayout.CENTER);
-//		panel.add(center_panel, BorderLayout.CENTER);
-//
-//		JPanel right_panel = new JPanel();
-//		right_panel.setBorder(new EmptyBorder(20, 10, 10, 10));
-//		right_panel.setLayout(new FlowLayout(FlowLayout.LEFT));
-//
-//		JPanel results_panel = new JPanel();
-//		results_panel.setLayout(new BorderLayout());
-//
-//		panelTitle = new JLabel("Para a configuração gerada, obtemos:");
-//		panelTitle.setFont(font_titles);
-//		results_panel.add(panelTitle, BorderLayout.NORTH);
-//
-//		JPanel help_panel = new JPanel();
-//		help_panel.setBorder(new EmptyBorder(10, 10, 10, 10));
-//		help_panel.setLayout(new GridLayout(0, 1));
-//		help_panel.add(createHelpPanel(1));
-//		help_panel.add(createHelpPanel(2));
-//
-//		results_panel.add(help_panel, BorderLayout.CENTER);
-//
-//		right_panel.add(results_panel);
-//
-//		panel.add(right_panel, BorderLayout.EAST);
 
 		JPanel buttons_panel = new JPanel();
 		buttons_panel.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -199,111 +159,6 @@ public class AfinacaoAutomatica {
 		afinacaoAuto.dispose();
 		HomePage.visible(true);
 	}
-
-//	/**
-//	 * Criar o painel scrollable com as regras e respetivos pesos
-//	 */
-//	private void createRulesPanel() {
-//		JPanel rules_panel = new JPanel();
-//		rules_panel.setLayout(new GridLayout(0, 1));
-//		for (HashMap.Entry<String, Double> entry : Utils.rules_weights.entrySet()) {
-//			JPanel panel = new JPanel();
-//			panel.setLayout(new BorderLayout());
-//			panel.add(new JLabel(entry.getKey() + "     "), BorderLayout.CENTER);
-//			panel.add(new JLabel(String.format("%.4f", entry.getValue())), BorderLayout.EAST);
-//			rules_panel.add(panel);
-//		}
-//		scroll_rules_panel = new JScrollPane(rules_panel);
-//	}
-//
-//	/**
-//	 * Criar o painel com a contagem de FP ou FN, o botão de ajuda e a respetiva
-//	 * textArea informativa
-//	 * 
-//	 * @param number
-//	 *            Indica qual dos dois cenários tratar (1 para FP e 2 para FN)
-//	 * @return
-//	 */
-//	private JPanel createHelpPanel(int number) {
-//		JPanel panel = new JPanel();
-//		panel.setBorder(new EmptyBorder(20, 10, 10, 10));
-//		panel.setLayout(new BorderLayout());
-//		panel.add(formatHelpButton(number), BorderLayout.WEST);
-//
-//		JLabel count = new JLabel();
-//		count.setFont(font_labels);
-//		panel.add(count, BorderLayout.CENTER);
-//
-//		JTextArea info = formatTextArea(" ");
-//		info.setFont(font_text);
-//		panel.add(info, BorderLayout.SOUTH);
-//
-//		if (number == 1) {
-//			help_label_fp = count;
-//			info.setText(Utils.newLine
-//					+ "Um Falso Positivo (FP) ocorre quando uma mensagem legítima é classificada como mensagem spam.");
-//			help_text_fp = info;
-//		} else {
-//			help_label_fn = count;
-//			info.setText(Utils.newLine
-//					+ "Um Falso Negativo (FN) ocorre quando uma mensagem spam é classificada como mensagem legítima.");
-//			help_text_fn = info;
-//		}
-//		return panel;
-//	}
-//
-//	/**
-//	 * Formatar a textArea informativa
-//	 * 
-//	 * @param info
-//	 *            Texto de ajuda
-//	 * @return textArea formatada
-//	 */
-//	private JTextArea formatTextArea(String info) {
-//		JTextArea textarea = new JTextArea(info);
-//		textarea.setForeground(new JPanel().getBackground());
-//		textarea.setBackground(new JPanel().getBackground());
-//		textarea.setLineWrap(true);
-//		textarea.setWrapStyleWord(true);
-//		textarea.setEditable(false);
-//		return textarea;
-//	}
-//
-//	/**
-//	 * Formatar os botões de ajuda
-//	 * 
-//	 * @param number
-//	 *            Número do botão a formatar
-//	 * @return botão de ajuda formatado
-//	 */
-//	private JButton formatHelpButton(int number) {
-//		JButton button = new JButton(new ImageIcon("./src/antiSpamFilter/frames/icons/help_button.png"));
-//		button.setMargin(new Insets(0, 0, 0, 0));
-//		button.setBorderPainted(false);
-//		button.setContentAreaFilled(false);
-//		button.setFocusPainted(false);
-//		button.setOpaque(false);
-//		button.addActionListener(new ActionListener() {
-//			@Override
-//			public void actionPerformed(ActionEvent e) {
-//				appearText(number == 1 ? help_text_fp : help_text_fn);
-//			}
-//		});
-//		return button;
-//	}
-//
-//	/**
-//	 * Caso o texto informativo esteja visível, camufla-o com o background. Caso
-//	 * contrário, torna-o visível.
-//	 * 
-//	 * @param infoText
-//	 */
-//	private void appearText(JTextArea infoText) {
-//		if (infoText.getForeground().equals(Color.BLACK))
-//			infoText.setForeground(new JPanel().getBackground());
-//		else
-//			infoText.setForeground(Color.BLACK);
-//	}
 
 	/**
 	 * Define a visibilidade da frame de Afinação Automática
--- src/antiSpamFilter/frames/Otimizacao.java
+++ src/antiSpamFilter/frames/Otimizacao.java
@@ -1,42 +1,32 @@
 package antiSpamFilter.frames;
 
 import java.awt.BorderLayout;
-//import java.awt.Color;
+import java.awt.Desktop;
 import java.awt.FlowLayout;
-//import java.awt.Font;
-//import java.awt.GridLayout;
-//import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.concurrent.Executor;
-//import javax.swing.ImageIcon;
+import java.util.concurrent.Executors;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
-//import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
-//import javax.swing.JScrollPane;
-//import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
-//import javax.swing.border.EmptyBorder;
 import antiSpamFilter.AntiSpamFilterAutomaticConfiguration;
 import antiSpamFilter.utils.GuiUtils;
 import antiSpamFilter.utils.Utils;
 
 public class Otimizacao {
 
-	private static JFrame frame;
-	// private Font font_titles = new Font("Helvetica", Font.PLAIN, 18),
-	// font_labels = new Font("Helvetica", Font.PLAIN, 14), font_text = new
-	// Font("Helvetica", Font.PLAIN, 12);
-	// private JTextArea help_area_fp, help_area_fn;
+	private static JFrame frame, progressFrame;
 	private String help_text_fp, help_text_fn;
 	private static final String algorithmOutputFilesPath = "./experimentBaseDirectory/referenceFronts/AntiSpamFilterProblem.r";
 
@@ -62,7 +52,7 @@ public class Otimizacao {
 		progressBar.setString("A calcular...");
 		progressBar.setStringPainted(true);
 		progressBar.setIndeterminate(true);
-		JFrame progressFrame = new JFrame();
+		progressFrame = new JFrame();
 		progressFrame.add(progressBar);
 		GuiUtils.frameAtCenter(progressFrame);
 		progressFrame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
@@ -71,8 +61,11 @@ public class Otimizacao {
 		progressFrame.setAlwaysOnTop(true);
 		progressFrame.setVisible(true);
 
-		Executor executor = java.util.concurrent.Executors.newSingleThreadExecutor();
-		executor.execute(new Runnable() {
+		Executors.newSingleThreadExecutor().execute(executeAlgorithm());
+	}
+
+	private Runnable executeAlgorithm() {
+		return new Runnable() {
 			@Override
 			public void run() {
 				try {
@@ -81,17 +74,35 @@ public class Otimizacao {
 					JOptionPane.showMessageDialog(progressFrame,
 							"Ocorreu um problema durante a execução da framework JMetal", "Erro",
 							JOptionPane.WARNING_MESSAGE);
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
 				SwingUtilities.invokeLater(new Runnable() {
 					@Override
 					public void run() {
+						try {
+							ProcessBuilder builder = new ProcessBuilder(new String[] { "Rscript", "./HV.Boxplot.R" });
+							builder.directory(new File("./experimentBaseDirectory/AntiSpamStudy/R/").getAbsoluteFile());
+							builder.start().waitFor();
+							File output = new File("./experimentBaseDirectory/AntiSpamStudy/R/HV.Boxplot.eps");
+							if (output.exists())
+								Desktop.getDesktop().open(output);
+							else
+								JOptionPane.showMessageDialog(progressFrame,
+										"aconteceu um problema a compilar com o Rscript.exe");
+						} catch (IOException | InterruptedException e) {
+							JOptionPane.showMessageDialog(progressFrame,
+									"aconteceu um problema a compilar com o Rscript.exe");
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
 						readAlgorithmOutputs();
-						progressFrame.dispose();
 						constructFrame();
+						progressFrame.dispose();
 					}
 				});
 			}
-		});
+		};
 	}
 
 	private void readAlgorithmOutputs() {
@@ -128,41 +139,6 @@ public class Otimizacao {
 	private void addContents() {
 		JPanel panel = new JPanel();
 		GuiUtils.constructGUI(panel, help_text_fp, help_text_fn);
-		// panel.setLayout(new BorderLayout());
-		// panel.setBorder(new EmptyBorder(20, 20, 20, 20));
-		//
-		// JPanel center_panel = new JPanel();
-		// center_panel.setLayout(new BorderLayout());
-		//
-		// JLabel panelTitle = new JLabel("Configuração do vetor de pesos");
-		// panelTitle.setFont(font_titles);
-		// center_panel.add(panelTitle, BorderLayout.NORTH);
-		//
-		// center_panel.add(createRulesPanel(), BorderLayout.CENTER);
-		// panel.add(center_panel, BorderLayout.CENTER);
-		//
-		// JPanel right_panel = new JPanel();
-		// right_panel.setBorder(new EmptyBorder(20, 10, 10, 10));
-		// right_panel.setLayout(new FlowLayout(FlowLayout.LEFT));
-		//
-		// JPanel results_panel = new JPanel();
-		// results_panel.setLayout(new BorderLayout());
-		//
-		// panelTitle = new JLabel("Para a configuração gerada, obtemos:");
-		// panelTitle.setFont(font_titles);
-		// results_panel.add(panelTitle, BorderLayout.NORTH);
-		//
-		// JPanel help_panel = new JPanel();
-		// help_panel.setBorder(new EmptyBorder(10, 10, 10, 10));
-		// help_panel.setLayout(new GridLayout(0, 1));
-		// help_panel.add(createHelpPanel(1));
-		// help_panel.add(createHelpPanel(2));
-		//
-		// results_panel.add(help_panel, BorderLayout.CENTER);
-		//
-		// right_panel.add(results_panel);
-		//
-		// panel.add(right_panel, BorderLayout.EAST);
 
 		JPanel buttons_panel = new JPanel();
 		buttons_panel.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -207,116 +183,6 @@ public class Otimizacao {
 
 		frame.add(panel);
 	}
-
-	// /**
-	// * Criar o painel scrollable com as regras e respetivos pesos
-	// *
-	// * @return
-	// */
-	// private JScrollPane createRulesPanel() {
-	// JPanel rules_panel = new JPanel();
-	// rules_panel.setLayout(new GridLayout(0, 1));
-	// for (HashMap.Entry<String, Double> entry :
-	// Utils.rules_weights.entrySet()) {
-	// JPanel panel = new JPanel();
-	// panel.setLayout(new BorderLayout());
-	// panel.add(new JLabel(entry.getKey() + " "), BorderLayout.CENTER);
-	// panel.add(new JLabel(String.format("%.4f", entry.getValue())),
-	// BorderLayout.EAST);
-	// rules_panel.add(panel);
-	// }
-	// return new JScrollPane(rules_panel);
-	// }
-	//
-	// /**
-	// * Criar o painel com a contagem de FP ou FN, o botão de ajuda e a
-	// respetiva
-	// * textArea informativa
-	// *
-	// * @param number
-	// * Indica qual dos dois cenários tratar (1 para FP e 2 para FN)
-	// * @return
-	// */
-	// private JPanel createHelpPanel(int number) {
-	// JPanel panel = new JPanel();
-	// panel.setBorder(new EmptyBorder(20, 10, 10, 10));
-	// panel.setLayout(new BorderLayout());
-	// panel.add(formatHelpButton(number), BorderLayout.WEST);
-	//
-	// JLabel count = new JLabel(number == 1 ? help_text_fp : help_text_fn);
-	// count.setFont(font_labels);
-	// panel.add(count, BorderLayout.CENTER);
-	//
-	// if (number == 1) {
-	// help_area_fp = formatTextArea(Utils.newLine
-	// + "Um Falso Positivo (FP) ocorre quando uma mensagem legítima é
-	// classificada como mensagem spam.");
-	// help_area_fp.setFont(font_text);
-	// panel.add(help_area_fp, BorderLayout.SOUTH);
-	// } else {
-	// help_area_fn = formatTextArea(Utils.newLine
-	// + "Um Falso Negativo (FN) ocorre quando uma mensagem spam é classificada
-	// como mensagem legítima.");
-	// help_area_fn.setFont(font_text);
-	// panel.add(help_area_fn, BorderLayout.SOUTH);
-	// }
-	// return panel;
-	// }
-	//
-	// /**
-	// * Formatar a textArea informativa
-	// *
-	// * @param info
-	// * Texto de ajuda
-	// * @return textArea formatada
-	// */
-	// private JTextArea formatTextArea(String info) {
-	// JTextArea textarea = new JTextArea(info);
-	// textarea.setForeground(new JPanel().getBackground());
-	// textarea.setBackground(new JPanel().getBackground());
-	// textarea.setLineWrap(true);
-	// textarea.setWrapStyleWord(true);
-	// textarea.setEditable(false);
-	// return textarea;
-	// }
-	//
-	// /**
-	// * Formatar os botões de ajuda
-	// *
-	// * @param number
-	// * Número do botão a formatar
-	// * @return botão de ajuda formatado
-	// */
-	// private JButton formatHelpButton(int number) {
-	// JButton button = new JButton(new
-	// ImageIcon("./src/antiSpamFilter/frames/icons/help_button.png"));
-	// button.setMargin(new Insets(0, 0, 0, 0));
-	// button.setBorderPainted(false);
-	// button.setContentAreaFilled(false);
-	// button.setFocusPainted(false);
-	// button.setOpaque(false);
-	// button.addActionListener(new ActionListener() {
-	// @Override
-	// public void actionPerformed(ActionEvent e) {
-	// appearText(number == 1 ? help_area_fp : help_area_fn);
-	// }
-	// });
-	// return button;
-	// }
-	//
-	// /**
-	// * Caso o texto informativo esteja visível, camufla-o com o background.
-	// Caso
-	// * contrário, torna-o visível.
-	// *
-	// * @param infoText
-	// */
-	// private void appearText(JTextArea infoText) {
-	// if (infoText.getForeground().equals(Color.BLACK))
-	// infoText.setForeground(new JPanel().getBackground());
-	// else
-	// infoText.setForeground(Color.BLACK);
-	// }
 
 	/**
 	 * Descarta a GUI atual e retorna à Home Page
